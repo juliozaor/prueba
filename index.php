a091006e73b64b5a100f7fe1ba94ad82add3f04e
Julio Jimenez <zaor.julio@gmail.com>
2022-06-30 20:17:35 -0500

    Primer despligue

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>
+            Prueba
+        </title>
+
+        </link>
+    </head>
+    <body>
+      <h1>Hola Mundo</h1>
+    </body>
+</html>
